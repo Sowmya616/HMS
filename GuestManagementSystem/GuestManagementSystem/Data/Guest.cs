da373a4d04a184de6da8b3d5cc405e1ad51d387c
sowmya616 <chsowmya161@gmail.com>
2022-09-13 19:01:24 +0530

    first commit

---
+++ GuestManagementSystem/GuestManagementSystem/Data/Guest.cs
@@ -0,0 +1,15 @@
+﻿using GuestManagementSystem.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GuestManagementSystem.Models
+{
+    [Table("Guest")]
+    public class Guest : GuestAbstract
+    {
+        public int Id { get; set; }
+    }
+
+}
+        {
+
+
